397f026a8d5a0d080aff6e24458e4572a2b2f6fc
uni-ezobafun <fukaya.s941109@gmail.com>
2021-10-31 12:35:45 +0900

    [update] ファイル分割

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import traceback
+
+from dummy_data_creator import create_dummy_data_queue
+from glue_spark_test.parallel_processor import process_in_parallel
+from glue_spark_test.non_parallel_processor import process_in_non_parallel
+
+# 分散処理させるか否か
+PARALLEL_PROCESSING = False
+
+def main():
+    pre_process_data_queue = create_dummy_data_queue()
+    if PARALLEL_PROCESSING:
+        process_in_parallel(pre_process_data_queue)
+    else:
+        process_in_non_parallel(pre_process_data_queue)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        traceback.print_exc()
