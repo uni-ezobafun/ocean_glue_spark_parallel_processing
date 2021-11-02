b9c07ae2e4c858f9fc06789735cef0f6ca9ed56c
uni-ezobafun <fukaya.s941109@gmail.com>
2021-11-02 21:49:39 +0900

    [update] pre_process_dataで命名統一

--- src/glue_spark_test/non_parallel_processor.py
+++ src/glue_spark_test/non_parallel_processor.py
@@ -3,6 +3,6 @@ from glue_spark_test.something_heavy_process.util import sort_by_bubble_sort
 def process_in_non_parallel(pre_process_data_queue):   
     whole_process_count = len(pre_process_data_queue)
 
-    for index, dummy_data in enumerate(pre_process_data_queue):
-        print(sort_by_bubble_sort(dummy_data))
+    for index, pre_process_data in enumerate(pre_process_data_queue):
+        print(sort_by_bubble_sort(pre_process_data))
         print(f"iteration_count: {index + 1}/{whole_process_count} completed")
