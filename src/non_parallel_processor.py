5e1e79800a1414b082e5af54480ae2be3059e8be
uni-ezobafun <fukaya.s941109@gmail.com>
2021-11-03 21:30:44 +0900

    [add] docs

--- src/non_parallel_processor.py
+++ src/non_parallel_processor.py
@@ -10,11 +10,25 @@ DUMMY_LIST_COUNT = 1000
 
 
 def main():
+    """AWS Glue Jobをキックして最初に呼ばれるエントリポイント 
+
+    Args:
+
+    Returns:
+
+    """
     pre_process_data_queue = create_dummy_data_queue()
     process_in_non_parallel(pre_process_data_queue)
 
 
 def create_dummy_data_queue():
+    """ランダム値を規定の個数だけ詰めた2次元リストを返却(ここでは5000列 x 1000行)
+
+    Args:
+
+    Returns:
+        dummy_data_queue: ランダム値の整数型リスト
+    """
     dummy_data_queue = []
     for i in range(DUMMY_LIST_COUNT):
         dummy_data = [randint(0,100000) for _ in range(ITEM_COUNT_IN_A_DUMMY_LIST)]
@@ -23,7 +37,15 @@ def create_dummy_data_queue():
     return dummy_data_queue
 
 
-def process_in_non_parallel(pre_process_data_queue):   
+def process_in_non_parallel(pre_process_data_queue):
+    """分散処理なしで愚直にループを回して結果を標準出力
+
+    Args:
+        pre_process_data_queue: 変換前データ(ランダム値の整数型リスト)
+
+    Returns:
+
+    """
     whole_process_count = len(pre_process_data_queue)
 
     for index, pre_process_data in enumerate(pre_process_data_queue):
@@ -32,6 +54,14 @@ def process_in_non_parallel(pre_process_data_queue):
 
 
 def sort_by_bubble_sort(num_list):
+    """重い処理の疑似としてバブルソート
+
+    Args:
+        num_list: ランダム値の整数型リスト
+
+    Returns:
+        num_list: 昇順ソート済の整数型リスト
+    """
     for m in range(len(num_list)):
         for n in range(len(num_list)-1, m, -1):
             if num_list[n] < num_list[n-1]:
--- src/parallel_processor.py
+++ src/parallel_processor.py
@@ -14,11 +14,25 @@ RDD_DIVISION_COUNT = 200
 
 
 def main():
+    """AWS Glue Jobをキックして最初に呼ばれるエントリポイント 
+
+    Args:
+
+    Returns:
+
+    """
     pre_process_data_queue = create_dummy_data_queue()
     process_in_parallel(pre_process_data_queue)
 
 
 def create_dummy_data_queue():
+    """ランダム値を規定の個数だけ詰めた2次元リストを返却(ここでは5000列 x 1000行)
+
+    Args:
+
+    Returns:
+        dummy_data_queue: ランダム値の整数型リスト
+    """
     dummy_data_queue = []
     for i in range(DUMMY_LIST_COUNT):
         dummy_data = [randint(0,100000) for _ in range(ITEM_COUNT_IN_A_DUMMY_LIST)]
@@ -28,6 +42,14 @@ def create_dummy_data_queue():
 
 
 def process_in_parallel(pre_process_data_queue):
+    """分散処理ありでデータ処理(rdd作成 -> sparkを呼ぶ -> 結果を標準出力)
+
+    Args:
+        pre_process_data_queue: 変換前データ(ランダム値の整数型リスト)
+
+    Returns:
+
+    """
     whole_process_count = len(pre_process_data_queue)
 
     rdd = organize_rdd(pre_process_data_queue)
@@ -38,6 +60,14 @@ def process_in_parallel(pre_process_data_queue):
 
 
 def organize_rdd(pre_process_data_queue):
+    """受け取ったデータをtupleに入れてrddを作る
+
+    Args:
+        pre_process_data_queue: 変換前データ(ランダム値の整数型リスト)
+
+    Returns:
+        rdd: レジリエントな分散データセット
+    """
     rdd_elements = []
     
     for index, pre_process_data in enumerate(pre_process_data_queue):
@@ -50,7 +80,20 @@ def organize_rdd(pre_process_data_queue):
     return rdd
 
 
+###
+### ↓ ここから下の処理が分散処理される ↓
+###
+
+
 def entry_spark_process(spark_arg_obj):
+    """分散処理のworker内で実行される処理のエントリポイント
+
+    Args:
+        spark_arg_obj: rddの1要素(tuple)
+
+    Returns:
+        dict: rdd生成時に振られたindexと変換後データの組
+    """
     index = spark_arg_obj[0]
     pre_process_data = spark_arg_obj[1]
 
@@ -65,6 +108,14 @@ def entry_spark_process(spark_arg_obj):
 
 
 def sort_by_bubble_sort(num_list):
+    """重い処理の疑似としてバブルソート
+
+    Args:
+        num_list: ランダム値の整数型リスト
+
+    Returns:
+        num_list: 昇順ソート済の整数型リスト
+    """
     for m in range(len(num_list)):
         for n in range(len(num_list)-1, m, -1):
             if num_list[n] < num_list[n-1]:
