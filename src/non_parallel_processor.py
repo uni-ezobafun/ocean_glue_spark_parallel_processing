1b1fc625525dacfc309e80ac6bd30c897eb1b7c0
uni-ezobafun <fukaya.s941109@gmail.com>
2021-11-03 17:15:44 +0900

    [fix] 修正漏れ

--- src/non_parallel_processor.py
+++ src/non_parallel_processor.py
@@ -27,7 +27,7 @@ def process_in_non_parallel(pre_process_data_queue):
     whole_process_count = len(pre_process_data_queue)
 
     for index, pre_process_data in enumerate(pre_process_data_queue):
-        print(util.sort_by_bubble_sort(pre_process_data))
+        print(sort_by_bubble_sort(pre_process_data))
         print(f"iteration_count: {index + 1}/{whole_process_count} completed")
 
 
